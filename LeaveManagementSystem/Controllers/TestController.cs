8030a258f05017751094e065d659a9eb38904b51
Calafia <Calafia@DESKTOP-TC6POO5>
2024-07-24 22:48:29 -0700

    Agregar archivos de proyecto.

---
+++ LeaveManagementSystem/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new TestViewModel
+            {
+                Name = "Saul",
+                DateOfBirth = new DateTime(1998, 03, 01)
+            };
+
+            return View(model);
+        }
+    }
+}
