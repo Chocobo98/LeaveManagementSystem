9b08852b14ddc33902dcfb633a104ff2d583f6dc
Calafia <osunasaul98@live.com>
2024-07-31 01:27:20 -0700

    Change Database name before create it by Update-Database command

--- LeaveManagementSystem/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace LeaveManagementSystem.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        //IdentityDbContext - Tiene ya definido y creado las tablas y roles
+        //DBContext - Crear los modelos, tablas y demas desde Scratch sin algun helper. Representa esta clase como la Base de datos
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
